d0ad22233b806cc2c4e440b366553573d058b241
Dawid Rogalski <roggalsky@gmail.com>
2023-10-16 14:27:37 +0200

    ruch-komputera

--- js/script.js
+++ js/script.js
@@ -7,6 +7,35 @@ function printMessage(msg){
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
-let computerMove = `kamień`;
-let playerMove = 'papier';
-printMessage('Zagrałem' +computerMove+ 'Jeśli Twój ruch to' + playerMove + 'to wygrywasz!');
+
+let randomFraction = Math.random();
+
+printMessage('Wylosowany ułamek to: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
+/** */
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+if(randomNumber == 2){
+	computerMove  = 'papier';
+}
+if(randomNumber == 3){
+	computerMove  = 'nozyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+/** */
