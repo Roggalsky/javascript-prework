8a09b1d0d8dd4ab1ba8427c9a008d3e697c1a689
Dawid Rogalski <roggalsky@gmail.com>
2023-10-16 19:56:30 +0200

    ruch-gracza

--- js/script.js
+++ js/script.js
@@ -3,11 +3,6 @@ function printMessage(msg){
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
 let randomFraction = Math.random();
 
 printMessage('Wylosowany ułamek to: ' + randomFraction);
@@ -19,8 +14,25 @@ printMessage('Ułamek pomnożony przez 3 i powiększony o 1: ' + calculation);
 let roundNumber = Math.floor(calculation);
 
 printMessage('Liczba po zaokrągleniu w dół to: ' + roundNumber);
-/** */
+/**========================================================================*/
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}else if (playerInput =='2'){
+	playerMove = 'papier'
+}else if (playerInput == '3'){
+	playerMove = 'nozyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+
+/**=========================================================================*/
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -29,13 +41,10 @@ let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
   computerMove = 'kamień';
-}
-if(randomNumber == 2){
-	computerMove  = 'papier';
-}
-if(randomNumber == 3){
-	computerMove  = 'nozyce';
+}	else if (randomNumber == 2){
+	computerMove = 'papier'
+} else if ( randomNumber == 3){
+	computerMove = 'nozyce'
 }
 
-printMessage('Mój ruch to: ' + computerMove);
-/** */
+printMessage('Ruch komoutera to: ' + computerMove);
